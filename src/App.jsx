a21c9c90d8ef373ca8dcf50b1897f23d64a2103f
MohamedZakidev <mr.joke2012.mz@gmail.com>
2024-05-31 12:42:55 +0300

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
-function App() {
+/* eslint-disable no-unused-vars */
+import { Button } from "@chakra-ui/react"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Home from "./pages/Home/Home"
 
+function App() {
   return (
     <>
-      <p>hi</p>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
