07fb138c3023e7e39960aab840a7fd9f81b23460
MohamedZakidev <mr.joke2012.mz@gmail.com>
2024-06-05 12:40:52 +0300

    add

--- src/components/FeedPost/FeedPost.jsx
+++ src/components/FeedPost/FeedPost.jsx
@@ -1,13 +1,33 @@
+import { Avatar, Button, Flex, Image, Text } from "@chakra-ui/react"
 import styles from "./feedpost.module.css"
 
 function FeedPost() {
     return (
         <div className={styles.feedPost}>
             <div className={styles.feedPostHeader}>
-                <div>dasdasdsa</div>
-                <div>dasdasdsa</div>
+                <Flex alignItems={"center"}>
+                    <Avatar src="img1.png" alt="user profile picture" size={"sm"} />
+                    <Text ml={3} fontSize={14} fontWeight={"600"}>Mohamed Zaki</Text>
+                    <Text ml={3} fontSize={12} color={"gray.500"}>• 1w </Text>
+                </Flex>
+                <div>
+                    <Button
+                        fontSize={14}
+                        color={"blue.500"}
+                        fontWeight={"bold"}
+                        _hover={{
+                            color: "white"
+                        }}
+                        bg={"none"}
+                        transition={"0.2s ease-in-out"}
+                    >
+                        Unfollow
+                    </Button>
+                </div>
             </div>
-            <div className={styles.feedPostBody}>feedpost body</div>
+            <div className={styles.feedPostBody}>
+                <Image src="img1.png" alt="user profile picture" />
+            </div>
             <div className={styles.feedPostFooter}>feedpost footer</div>
         </div>
     )
