34823ac93dafb90618513ccfdb41d8d6e4532a9f
MohamedZakidev <mr.joke2012.mz@gmail.com>
2024-06-05 14:42:09 +0300

    add

--- src/components/FeedPost/FeedPost.jsx
+++ src/components/FeedPost/FeedPost.jsx
@@ -1,13 +1,30 @@
-import { Avatar, Button, Flex, Image, Text } from "@chakra-ui/react"
+import { Avatar, Box, Button, Flex, Image, Text } from "@chakra-ui/react"
 import styles from "./feedpost.module.css"
+import { CommentLogo, NotificationsLogo, UnlikeLogo } from "../../../public/assets/constants"
+import { useState } from "react"
 
 function FeedPost() {
+    const [isLiked, setIsLiked] = useState(false)
+    const [likesCount, setLikesCount] = useState(0)
+    console.log(isLiked);
+
+    function handleLikes() {
+        console.log("click");
+        if (isLiked) {
+            setIsLiked(false)
+            setLikesCount(prev => prev - 1)
+        } else {
+            setIsLiked(true)
+            setLikesCount(prev => prev + 1)
+        }
+    }
+
     return (
         <div className={styles.feedPost}>
             <div className={styles.feedPostHeader}>
                 <Flex alignItems={"center"}>
                     <Avatar src="img1.png" alt="user profile picture" size={"sm"} />
-                    <Text ml={3} fontSize={14} fontWeight={"600"}>Mohamed Zaki</Text>
+                    <Text ml={3} fontSize={14} fontWeight={"600"}>Ana de armas</Text>
                     <Text ml={3} fontSize={12} color={"gray.500"}>• 1w </Text>
                 </Flex>
                 <div>
@@ -28,7 +45,19 @@ function FeedPost() {
             <div className={styles.feedPostBody}>
                 <Image src="img1.png" alt="user profile picture" />
             </div>
-            <div className={styles.feedPostFooter}>feedpost footer</div>
+            <div className={styles.feedPostFooter}>
+                <Flex gap={4}>
+                    <Box onClick={handleLikes} cursor={"pointer"}>
+                        {!isLiked ? <NotificationsLogo /> : <UnlikeLogo />}
+                    </Box>
+                    <Box cursor={"pointer"}>
+                        <CommentLogo />
+                    </Box>
+                </Flex>
+                <div>user name and caption</div>
+                <div>veiw all 100 comments</div>
+                <div>input for comment</div>
+            </div>
         </div>
     )
 }
