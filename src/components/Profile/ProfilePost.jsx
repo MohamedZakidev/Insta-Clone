bfb2d41c50db2e3b9556b8d7cdb9feae90f60593
MohamedZakidev <mr.joke2012.mz@gmail.com>
2024-06-13 18:27:31 +0300

    add

--- src/components/Profile/ProfilePost.jsx
+++ src/components/Profile/ProfilePost.jsx
@@ -1,44 +1,57 @@
-import { Flex, GridItem, Image, Text } from "@chakra-ui/react"
+import { Button, Flex, GridItem, Image, Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay, Text, useDisclosure } from "@chakra-ui/react"
 import { AiFillHeart } from "react-icons/ai"
 import { FaComment } from "react-icons/fa"
 
-// eslint-disable-next-line react/prop-types
 function ProfilePost({ img }) {
+    const { isOpen, onOpen, onClose } = useDisclosure()
     return (
-        <GridItem
-            position={"relative"}
-            cursor={"pointer"}
-            border={"1px solid"}
-            borderColor={"whiteAlpha.300"}
-            borderRadius={4}
-            aspectRatio={1 / 1}
-        >
-            <Flex
-                opacity={0}
-                _hover={{ opacity: 1 }}
-                position={"absolute"}
-                bg={"blackAlpha.700"}
-                top={0}
-                bottom={0}
-                right={0}
-                left={0}
-                transition={"all 0.3s"}
-                justifyContent={"center"}
-                alignItems={"center"}
-                gap={6}
+        <>
+            <GridItem
+                position={"relative"}
+                cursor={"pointer"}
+                border={"1px solid"}
+                borderColor={"whiteAlpha.300"}
+                borderRadius={4}
+                aspectRatio={1 / 1}
+                onClick={onOpen}
             >
-                <Flex alignItems={"center"}>
-                    <AiFillHeart size={20} />
-                    <Text fontWeight={"bold"} ml={2}>7</Text>
+                <Flex
+                    opacity={0}
+                    _hover={{ opacity: 1 }}
+                    position={"absolute"}
+                    bg={"blackAlpha.700"}
+                    top={0}
+                    bottom={0}
+                    right={0}
+                    left={0}
+                    transition={"all 0.3s"}
+                    justifyContent={"center"}
+                    alignItems={"center"}
+                    gap={6}
+                >
+                    <Flex alignItems={"center"}>
+                        <AiFillHeart size={20} />
+                        <Text fontWeight={"bold"} ml={2}>7</Text>
+                    </Flex>
+                    <Flex alignItems={"center"}>
+                        <FaComment size={20} />
+                        <Text fontWeight={"bold"} ml={2}>7</Text>
+                    </Flex>
                 </Flex>
-                <Flex alignItems={"center"}>
-                    <FaComment size={20} />
-                    <Text fontWeight={"bold"} ml={2}>7</Text>
-                </Flex>
-            </Flex>
-            <Image src={img} w={"full"} h={"full"} objectFit={"cover"} alt="profile post" />
+                <Image src={img} w={"full"} h={"full"} objectFit={"cover"} alt="profile post" />
+            </GridItem>
+            <Modal isOpen={isOpen} onClose={onClose}>
+                <ModalOverlay />
+                <ModalContent>
+                    <ModalHeader>Modal Title</ModalHeader>
+                    <ModalCloseButton />
+                    <ModalBody>
+                        this is a modal
+                    </ModalBody>
+                </ModalContent>
+            </Modal>
+        </>
 
-        </GridItem>
     )
 }
 
