a844193661900d93d731ff78ed783829691ecdd3
MohamedZakidev <mr.joke2012.mz@gmail.com>
2024-06-27 02:49:31 +0300

    Update Layout.jsx

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,13 +2,17 @@ import { Flex } from "@chakra-ui/react"
 import { Outlet } from "react-router-dom"
 import Sidebar from "./Sidebar"
 import { useLocation } from "react-router-dom"
+import { useAuthState } from "react-firebase-hooks/auth"
+import { auth } from "../firebase/firebase"
 
 function Layout() {
     const { pathname } = useLocation()
+    const [user] = useAuthState(auth)
 
+    const canRenderSidebar = pathname !== "/auth" && user
     return (
         <Flex>
-            {pathname === "/auth" ? null : <Sidebar />}
+            {canRenderSidebar ? <Sidebar /> : null}
             <Outlet />
         </Flex>
     )
